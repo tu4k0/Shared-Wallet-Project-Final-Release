d9d18c5321c453100539e1fd36179361ccb006bd
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-04-15 12:03:19 +0300

    Shared-Wallet-Project-Final-Release.sol ver 0.6
    Added mapping for allowance.
    Added OwnerORAllowed modifier and setAllowance function.

--- src/Shared-Wallet-Project-Final-Release.sol
+++ src/Shared-Wallet-Project-Final-Release.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (access/Ownable.sol)
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
@@ -5,14 +7,27 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract SharedWallet is Ownable
 {
 
+    mapping(address=>uint) public allowance;
+
+    function setAllowance(address _who, uint _amount) public onlyOwner
+    {
+        allowance[_who] = _amount;
+    }
+
+    modifier OwnerORAllowed(uint _amount)
+    {
+        require(owner() == msg.sender || allowance[msg.sender] > _amount, "You are not allowed");
+        _;
+    }
+
     fallback () external payable
     {
 
     }
 
-    function Withdraw(address payable _to, uint _amount) public onlyOwner
+    function Withdraw(address payable _to, uint _amount) public OwnerORAllowed(_amount)
     {
         _to.transfer(_amount);
     } 
-    
+
 }
