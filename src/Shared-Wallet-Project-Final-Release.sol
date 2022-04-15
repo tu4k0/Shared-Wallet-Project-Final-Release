9eaec1a20c1fd91e17a9f5a4198fe3611efd6d88
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-04-15 18:13:57 +0300

    Shared-Wallet-Project-Final-Release.sol ver 0.11
    Added events for SharedWallet contract to check detail information about money sent and money received.

--- src/Shared-Wallet-Project-Final-Release.sol
+++ src/Shared-Wallet-Project-Final-Release.sol
@@ -32,9 +32,12 @@ contract Allowance is Ownable
 contract SharedWallet is Allowance
 {
 
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     fallback () external payable
     {
-
+        emit MoneyReceived(msg.sender, msg.value);
     }
 
     function Withdraw(address payable _to, uint _amount) public OwnerORAllowed(_amount)
@@ -45,6 +48,7 @@ contract SharedWallet is Allowance
             reduceAllowance(msg.sender, _amount);
         }
         _to.transfer(_amount);
+        emit MoneySent(_to, _amount);
     } 
 
 }
