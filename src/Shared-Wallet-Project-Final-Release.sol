7d8bf401ffe5aed1dcdd3c0d2ae0eb3e912684a6
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-04-15 11:00:40 +0300

    Shared-Wallet-Project-Final-Release.sol ver 0.1
    Added function withdraw() for transfer money to another account.

--- src/Shared-Wallet-Project-Final-Release.sol
+++ src/Shared-Wallet-Project-Final-Release.sol
@@ -5,5 +5,10 @@ contract SharedWallet
     function () external payable
     {
 
+    }
+
+    function Withdraw(address payable _to, uint _amount) public
+    {
+        _to.transfer(_amount);
     } 
 }
