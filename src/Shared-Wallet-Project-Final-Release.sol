62a90b60551f70edf0f1faf5135b1cfad1538a9e
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-04-15 18:30:04 +0300

    Shared-Wallet-Project-Final-Release.sol ver 0.12
    Added SafeMath.sol library for Allowance contract for right and safe defining operations of substracting and adding.

--- src/Shared-Wallet-Project-Final-Release.sol
+++ src/Shared-Wallet-Project-Final-Release.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 contract Allowance is Ownable
 {
+    using SafeMath for uint;
+
     event AllowanceDetails(address indexed _fromWho, address indexed _toWho, uint _oldAmount, uint _newAmount);
 
     mapping(address=>uint) public allowance;
@@ -24,8 +27,8 @@ contract Allowance is Ownable
 
     function reduceAllowance(address _who, uint _amount) internal
     {
-        emit AllowanceDetails(msg.sender, _who, allowance[_who], allowance[_who] - _amount);
-        allowance[_who] -= _amount;
+        emit AllowanceDetails(msg.sender, _who, allowance[_who], allowance[_who].sub(_amount));
+        allowance[_who] = allowance[_who].sub(_amount);
     }
 }
 
